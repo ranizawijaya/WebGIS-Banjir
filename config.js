fd6e275fa251c207c869fff92faf0737b94cf183
ranizawijaya <wijayaraniza@gmail.com>
2026-05-25 17:19:36 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Konfigurasi Global Aplikasi
- * Ubah BASE_URL di bawah ini jika endpoint server backend Anda berubah.
- */
-const CONFIG = {
-    BASE_URL: "http://wnyrjgvzpxlu.shares.zrok.io/webhook"
-};
