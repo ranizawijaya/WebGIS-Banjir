1a7996b8bf08948b7975627940502aae194de44c
ranizawijaya <wijayaraniza@gmail.com>
2026-05-25 13:05:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  * Ubah BASE_URL di bawah ini jika endpoint server backend Anda berubah.
  */
 const CONFIG = {
-    BASE_URL: "http://localhost:5678/webhook"
+    BASE_URL: "http://2iyzahb9rsp8.shares.zrok.io/webhook"
 };
