a98dc8243dc0efedacc47051d34926b7ade01490
ranizawijaya <wijayaraniza@gmail.com>
2026-05-25 12:52:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * Konfigurasi Global Aplikasi
+ * Ubah BASE_URL di bawah ini jika endpoint server backend Anda berubah.
+ */
+const CONFIG = {
+    BASE_URL: "http://localhost:5678/webhook"
+};
